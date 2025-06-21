60b366a5944cd933d68f596085dadc899e8744a2
Timothy Odebunmi <dev.brymes@gmail.com>
2025-06-21 17:31:31 +0100

    [Fix]: Update Backend URL

--- src/utils/httpRequest.ts
+++ src/utils/httpRequest.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getData } from "./storage";
 
 const httprequest = axios.create({
-  baseURL: "https://cp-backend-985687851836.us-central1.run.app/api/",
+  baseURL: "https://atlas.corporatesandprofessionals.com/api/",
 });
 
 // check if authtoken has been, if not, get it from the localstorage and attach it
